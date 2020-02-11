d8221752382511bd4ffdd5e4de3cf5363e5feca3
Alexander <aleksandrcw@gmail.com>
2020-02-11 21:00:08 +0300

    Plane and CoreMotion

--- WarFly/PlayerPlane.swift
+++ WarFly/PlayerPlane.swift
@@ -9,7 +9,12 @@
 import SpriteKit
 
 class PlayerPlane: SKSpriteNode {
-    static func populate(at point: CGFloat) -> SKSpriteNode {
-        return SKSpriteNode()
+    static func populate(at point: CGPoint) -> SKSpriteNode {
+        let playerPlaneTexture = SKTexture(imageNamed: "airplane_3ver2_13")
+        let playerPlane = SKSpriteNode(texture: playerPlaneTexture)
+        playerPlane.setScale(0.5)
+        playerPlane.position = point
+        playerPlane.zPosition = 20
+        return playerPlane
     }
 }
