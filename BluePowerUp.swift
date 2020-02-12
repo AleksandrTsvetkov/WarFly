bfeee9749a47cea195bd82f4a440666823579d1c
Alexander <aleksandrcw@gmail.com>
2020-02-13 01:20:59 +0300

    PowerUp classes

---
+++ BluePowerUp.swift
@@ -0,0 +1,20 @@
+//
+//  BluePowerUp.swift
+//  WarFly
+//
+//  Created by Александр Цветков on 13.02.2020.
+//  Copyright © 2020 Александр Цветков. All rights reserved.
+//
+
+import SpriteKit
+
+class BluePowerUp: PowerUp {
+    init() {
+        let textureAtlas = SKTextureAtlas(named: "BluePowerUp")
+        super.init(textureAtlas: textureAtlas)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ WarFly/Models/GreenPowerUp.swift
@@ -0,0 +1,20 @@
+//
+//  GreenPowerUp.swift
+//  WarFly
+//
+//  Created by Александр Цветков on 13.02.2020.
+//  Copyright © 2020 Александр Цветков. All rights reserved.
+//
+
+import SpriteKit
+
+class GreenPowerUp: PowerUp {
+    init() {
+        let textureAtlas = SKTextureAtlas(named: "GreenPowerUp")
+        super.init(textureAtlas: textureAtlas)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
